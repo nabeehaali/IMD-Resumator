97b5aaff55821328600fa5626fcdd22ae814cbb5
Nabeeha Ali <alinabeeha@gmail.com>
2022-11-22 10:13:44 -0500

    Fixed homepage

--- client/src/SubPages/Home.js
+++ client/src/SubPages/Home.js
@@ -1,4 +1,5 @@
-import { Button, Toolbar, Box, Typography, AppBar } from '@mui/material';
+import { Button, Toolbar, Box, Typography, AppBar, Stack } from '@mui/material';
+import './Home.css'
 
 function getStarted()
 {
@@ -10,13 +11,22 @@ function getStarted()
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     IMD Resumator
                 </Typography>
+                <Stack direction="row" spacing={2}>
                 <Button color="inherit" href="/">Home</Button>
                 <Button color="inherit" href="/Form">Templates</Button>
                 <Button color="inherit" href="/Instruction">How To</Button>
+                </Stack>
                 </Toolbar>
             </AppBar>
         </Box>
+        <div class="welcome">
         <h1>Welcome to IMD Resumator!</h1>
+        <p>This website is the perfect tool for IMD students to build a custom made resume for when they apply to any jobs in tech!</p>
+        <Button variant="contained" href="/Form">Create Your Resume</Button>
+        </div>
+        <div class="welcome-img">
+            <img src="https://www.w3schools.com/images/picture.jpg" alt="Italian Trulli"/>
+        </div>
         </div>
     )
 }
